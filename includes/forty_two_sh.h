f2a8837024ac443772d708f3c939df38efa7feff
Youssef Lagtab <yousseflagtab@gmail.com>
2021-03-22 10:23:34 +0100

    include by relative paths. fix prototype

--- includes/forty_two_sh.h
+++ includes/forty_two_sh.h
@@ -6,19 +6,19 @@
 /*   By: ylagtab <ylagtab@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 10:17:07 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/03/21 11:26:46 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/03/22 09:33:22 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FORTY_TWO_SH_H
 # define FORTY_TWO_SH_H
 
-# include "libft.h"
+# include "../libft/libft.h"
 # include "constants.h"
 # include "typedefs.h"
-# include "src/errors/errors.h"
-# include "src/parser/parser.h"
+# include "../src/errors/errors.h"
+# include "../src/parser/parser.h"
 
-void	del_token(t_token *token);
+void	del_token(void *element);
 
 #endif
