ca30c917d1271ffcd985a544a36d8ca5a82df80e
Youssef Lagtab <yousseflagtab@gmail.com>
2021-03-30 17:55:48 +0100

    assign cmd_type correctly. reset cmd_type

--- src/parser/parse_complete_commands/add_complete_cmd.c
+++ src/parser/parse_complete_commands/add_complete_cmd.c
@@ -6,7 +6,7 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/28 12:02:07 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/03/28 16:54:45 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/03/30 17:49:33 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,13 @@ int			add_complete_cmd(t_parse_complete *parser)
 	t_command *cmd;
 
 	cmd = (t_command*)ft_malloc(sizeof(t_command));
-	parser->cmd_type = parser->cmd_type;
+	cmd->type = parser->cmd_type;
 	cmd->is_background_job = parser->current_token.type == AMPERSAND;
 	cmd->command = get_command_by_type(parser);
 	if (cmd->command == NULL)
 		return (EXIT_FAILURE);
 	vector_push(parser->commands, cmd);
 	reset_cmd_tokens(parser);
+	parser->cmd_type = SIMPLE_CMD;
 	return (EXIT_SUCCESS);
 }
--- src/parser/print_commands.c
+++ src/parser/print_commands.c
@@ -6,7 +6,7 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 13:27:57 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/03/30 13:50:20 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/03/30 17:54:28 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ static void	print_simple_cmd(t_simple_command *cmd)
 	t_redirection	*reds;
 	size_t			i;
 
+	ft_printf(1, "\n------------------------------------------------");
 	ft_printf(1, "\n- simple_command:");
 	ft_printf(1, "\n-- assignments: ");
 	i = 0;
@@ -45,7 +46,42 @@ static void	print_simple_cmd(t_simple_command *cmd)
 			g_tokens_to_string[reds[i].type], reds[i].righ_fd);
 		++i;
 	}
-	ft_printf(1, "\n");
+	ft_printf(1, "\n------------------------------------------------\n");
+}
+
+void	print_pipe_cmd(t_pipe_sequence *cmd)
+{
+	t_simple_command	*simple_commands;
+	size_t				i;
+
+	ft_printf(1, "\n************************************************");
+	ft_printf(1, "\n* pipe_sequence:");
+	i = 0;
+	simple_commands = cmd->commands->array;
+	while (i < cmd->commands->length)
+	{
+		print_simple_cmd(simple_commands + i);
+		++i;
+	}
+	ft_printf(1, "\n************************************************\n");
+}
+
+void	print_logic_cmd(t_logic_sequence *cmd)
+{
+	t_token_type	*logic_ops;
+	size_t			i;
+
+	ft_printf(1, "\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	ft_printf(1, "\n@ logic_sequence:");
+	i = 0;
+	logic_ops = cmd->logic_ops->array;
+	while (i < cmd->logic_ops->length)
+	{
+		ft_printf(1, "%s ", g_tokens_to_string[logic_ops[i]]);
+		++i;
+	}
+	print_commands(cmd->commands);
+	ft_printf(1, "\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
 }
 
 void	print_commands(t_vector *commands_vec)
@@ -57,8 +93,13 @@ void	print_commands(t_vector *commands_vec)
 	i = 0;
 	while (i < commands_vec->length)
 	{
+		ft_printf(1, "is_background_job: %d", commands[i].is_background_job);
 		if (commands[i].type == SIMPLE_CMD)
 			print_simple_cmd(commands[i].command);
+		if (commands[i].type == PIPE_SEQ)
+			print_pipe_cmd(commands[i].command);
+		if (commands[i].type == LOGIC_SEQ)
+			print_logic_cmd(commands[i].command);
 		++i;
 	}
 }
