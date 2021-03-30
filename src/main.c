e50416ff90a900a9b13f8e16565f6a3be0725a13
Youssef Lagtab <yousseflagtab@gmail.com>
2021-03-30 16:25:57 +0100

    add -c option

--- src/main.c
+++ src/main.c
@@ -6,20 +6,26 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 10:15:48 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/03/28 16:56:42 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/03/30 12:55:19 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "forty_two_sh.h"
 
-int	main(int ac, char **av, char **envp)
+int			main(int ac, char *av[], char *envp[])
 {
-	(void)ac;
-	(void)av;
+	char *cmd;
+
 	(void)envp;
-	if (ac == 2)
-		(void)parser(av[1]);
+	signal(SIGTSTP, SIG_IGN);
+	if (ac == 3 && ft_strcmp(av[1], "-c") == 0)
+		(void)parser(av[2]);
+	else if (ac == 1)
+	{
+		if (get_next_line(STDIN_FILENO, &cmd) != -1)
+			(void)parser(cmd);
+	}
 	else
-		ft_printf(2, "error");
+		ft_printf(2, "Error\nusage: ./21sh [-c command]\n");
 	return (0);
 }
