323895ce33525430e52e68edfb08ec9698b2862f
Youssef Lagtab <yousseflagtab@gmail.com>
2021-04-24 15:19:33 +0000

    fix parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 10:28:42 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/04/22 14:54:46 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/04/24 15:17:23 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@ t_vector	*parser(char *line)
 	t_lexer_ret	*lex_ret;
 	t_vector	*commands;
 
-	(void)line;
-	lex_ret = lexer(line, FALSE);
-	if (lex_ret->is_matched == FALSE)
+	lex_ret = lexer(line);
+	if (lex_ret->is_matched == FALSE && lex_ret->unmatched_char != BACK_SLASH)
 	{
 		unmatch_quote_error(lex_ret->unmatched_char);
 		vector_free(lex_ret->tokens);
@@ -28,6 +27,7 @@ t_vector	*parser(char *line)
 	lexer_add_newline_token(lex_ret->tokens);
 	commands = parse_complete_commands(lex_ret->tokens);
 	vector_free(lex_ret->tokens);
+	free(lex_ret);
 	if (commands)
 		print_commands(commands);
 	return (commands);
