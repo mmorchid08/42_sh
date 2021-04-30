ca5c3b98ba3e863707aa76cfda5419167f4b58e9
Hatim Mzah <hmzah@e1r13p1.1337.ma>
2021-04-30 09:11:17 +0000

    Updated getting the full_path of the cmd

--- src/execution/exec_tools.c
+++ src/execution/exec_tools.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_tools.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mel-idri <mel-idri@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: hmzah <hmzah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 16:11:49 by hmzah             #+#    #+#             */
-/*   Updated: 2021/04/29 16:55:14 by mel-idri         ###   ########.fr       */
+/*   Updated: 2021/04/30 09:11:00 by hmzah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,18 @@ int	check_file(char *path)
 	if (access(path, F_OK) != -1)
 		return (1);
 	return (0);
+}
+
+int	check_file_permissions(char *path)
+{
+	struct stat st;
+	
+	if (!check_file(path)
+		|| access(path, X_OK) == -1
+		|| lstat(path, &st) == -1
+		|| S_ISDIR(st.st_mode))
+		return (0);
+	return (1);
 }
 
 char	*try_every_possibility(char *cmd, char *path_env)
@@ -37,7 +49,7 @@ char	*try_every_possibility(char *cmd, char *path_env)
 		tmp = ft_strjoin(ab[i], "/");
 		tmp2 = ft_strjoin(tmp, cmd);
 		free(tmp);
-		if (check_file(tmp2))
+		if (check_file_permissions(tmp2))
 		{
 			ft_free_strings_array(ab);
 			return (tmp2);
