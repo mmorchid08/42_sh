4e8337ba0aedc8283006a6f88bc9ccc4a07b8203
Youssef Lagtab <yousseflagtab@gmail.com>
2021-04-30 14:40:59 +0000

    add sh_system

--- src/main.c
+++ src/main.c
@@ -3,27 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmzah <hmzah@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 10:15:48 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/04/30 14:22:08 by hmzah            ###   ########.fr       */
+/*   Updated: 2021/04/30 14:43:48 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "forty_two_sh.h"
 
-t_vector
-*g_stopped_jobs;
-
-int
-g_exit_status;
-
-int
-g_term_fd;
-
-t_hash_table
-*g_hash;
-
+t_hash_table	*g_hash;
+t_vector		*g_stopped_jobs;
+int				g_exit_status;
+int				g_term_fd;
 
 int	shell_main(void)
 {
@@ -42,24 +34,38 @@ int	shell_main(void)
 	return (EXIT_SUCCESS);
 }
 
+int	sh_system(char *line)
+{
+	t_vector	*commands;
+
+	if (line == NULL)
+		return (-1);
+	commands = parser(line);
+	if (commands && commands->length > 0)
+		execute_commands(commands);
+	return (g_exit_status);
+}
+
 int	main(int ac, char *av[], char *envp[])
 {
 	(void)av;
 	g_hash = new_hash_table(100, 100);
 	g_shell_env = env_init(envp);
-	g_term_fd = open("/dev/tty", O_RDWR);
-	if (g_term_fd == -1)
-		ft_strerror(EOPENFILE,"open", NULL, TRUE);
-	init_jobs();
 	signal(SIGTTOU, SIG_IGN);
 	signal(SIGTTIN, SIG_IGN);
 	signal(SIGTSTP, SIG_IGN);
+	init_jobs();
 	if (ac == 1)
 	{
+		g_term_fd = open("/dev/tty", O_RDWR);
+		if (g_term_fd == -1)
+			ft_strerror(EOPENFILE, "open", NULL, TRUE);
 		while (1)
 			(void)shell_main();
 	}
+	else if (ac == 2)
+		return (sh_system(av[1]));
 	else
-		ft_printf(2, "Error\nusage: ./21sh [-c command]\n");
+		ft_printf(2, "Error\nusage: ./42sh  [command]\n");
 	return (0);
 }
