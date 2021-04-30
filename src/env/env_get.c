3531f9d9299a53b8f5f259d584e430b63507de5d
Youssef Lagtab <yousseflagtab@gmail.com>
2021-04-30 15:16:38 +0000

    add $? and $$

--- src/env/env_get.c
+++ src/env/env_get.c
@@ -3,14 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   env_get.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmzah <hmzah@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 08:57:08 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/04/30 12:17:18 by hmzah            ###   ########.fr       */
+/*   Updated: 2021/04/30 15:14:59 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "internals.h"
+
+char	*get_exit_status_str(void)
+{
+	static char	status[10];
+	char		*st;
+
+	st = ft_itoa(g_exit_status);
+	ft_strcpy(status, st);
+	status[ft_strlen(st)] = '\0';
+	free(st);
+	return (status);
+}
+
+char	*get_pid_str(void)
+{
+	static char	pid[11];
+	char		*p;
+
+	p = ft_itoa((int)getpid());
+	ft_strcpy(pid, p);
+	pid[ft_strlen(p)] = '\0';
+	free(p);
+	return (pid);
+}
 
 char	*escape_string(char *str)
 {
@@ -34,6 +58,10 @@ char	*env_get(t_vector *env, char *key)
 	t_var	*vars;
 	size_t	i;
 
+	if (ft_strequ(key, "?"))
+		return (get_exit_status_str());
+	if (ft_strequ(key, "$"))
+		return (get_pid_str());
 	i = 0;
 	vars = env->array;
 	while (i < env->length)
